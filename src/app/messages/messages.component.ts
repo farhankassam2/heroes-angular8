3e62faa4a6066e581f0ab0659f8236d235088f2e
Farhan Kassam <farhankassam@dbaresults.com>
2021-10-21 16:04:33 -0500

    testing pretty-quick now

--- src/app/messages/messages.component.ts
+++ src/app/messages/messages.component.ts
@@ -2,9 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { MessageService } from '../message.service';
 
 @Component({
-    selector   : 'app-messages',
+    selector: 'app-messages',
     templateUrl: './messages.component.html',
-    styleUrls  : ['./messages.component.css']
+    styleUrls: ['./messages.component.css']
 })
 export class MessagesComponent {
     /*
